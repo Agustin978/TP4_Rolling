98a13377796a1a991402751f418c02d0d196a90e
Agustin978 <loboagus10@gmail.com>
2023-03-16 00:38:12 -0300

    Desarrollo del ejercicio 1. Se crea un objeto auto con todos sus atributos ya ingresados.

--- Desarrollo/js/Ejercicio1.js
+++ Desarrollo/js/Ejercicio1.js
@@ -15,9 +15,9 @@ let auto =
 
 document.write("<section class='container'> <div class='card my-2'> <ul class='list-group list-group-flush'>");
 console.log(auto);
-document.write(auto.encender);
+document.write(auto.encender());
 document.write("<li class='list-group-item'><p>La marca del auto es "+auto.marca+".</p></li>");
 document.write("<li class='list-group-item'><p>El modelo del auto es "+auto.modelo+".</p></li>");
 document.write("<li class='list-group-item'><p>El auto es de color "+auto.color+".</p></li>");
-document.write(auto.apagar);
+document.write(auto.apagar());
 document.write("</ul></div></section>");
